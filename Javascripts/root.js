352c8cf59991594136456d60da31e487326c4d02
Michael Doliner <msd83@cornell.edu>
2015-01-22 09:47:32 -0500

    Times out transitions better, adds footer.

--- Javascripts/root.js
+++ Javascripts/root.js
@@ -5,26 +5,29 @@
   }
 
   MD.startUp = function () {
-    $(".logo").animate({"opacity": "1"}, 2000, "swing", function () {
-      $("#root-nav").animate({"top": "0", "opacity": "1"}, 450, "swing", function () {
-        $("#portfolio-nav").animate({"top": "0", "opacity": "1"}, 450, "swing", function () {
-          $("#resume-nav").animate({"top": "0", "opacity": "1"}, 450, "swing", function () {
-            $("#contact-nav").animate({"top": "0", "opacity": "1"}, 450, "swing", function () {
+    $(".logo").animate({"opacity": "1"}, 2200, "swing", function () {
+    });
+    setTimeout(function () {
+      $("#root-nav").animate({"top": "0", "opacity": "1"}, 410, "swing", function () {
+        $("#portfolio-nav").animate({"top": "0", "opacity": "1"}, 410, "swing", function () {
+          $("#resume-nav").animate({"top": "0", "opacity": "1"}, 410, "swing", function () {
+            $("#contact-nav").animate({"top": "0", "opacity": "1"}, 410, "swing", function () {
               $("#content").html($("#root").clone());
               $("#content").animate({"opacity": "1"}, 1000);
+              $(".footer").animate({"opacity": "1"}, 1000);
             })
           })
         })
       })
-    });
+    }, 550)
   };
 
   MD.switchContent = function (selector) {
     if (MD.currentSel !== selector) {
       MD.currentSel = selector;
-      $("#content").animate({"opacity": "0"}, 750, function () {
+      $("#content").animate({"opacity": "0"}, 400, function () {
         $("#content").html($(selector).clone());
-        $("#content").animate({"opacity": "1"}, 750);
+        $("#content").animate({"opacity": "1"}, 400);
       });
     }
   };
